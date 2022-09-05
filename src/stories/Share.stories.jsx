1a2cec844aed1e387aac5917d7dfa86fa30f7845
bhatshravan <shrvnbhat0@gmail.com>
2022-09-05 10:12:36 +0530

    Update story

--- src/stories/Share.stories.jsx
+++ src/stories/Share.stories.jsx
@@ -47,4 +47,41 @@ export default {
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template = (args) => <Share {...args} />;
 
-export const Share = Template.bind({});
+export const ShareButton = Template.bind({});
+
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+ShareButton.args = {
+  data: [
+    {
+      image: "/Logo.png",
+      name: "Everyone at Oslash",
+      members: "25",
+      email: "oslash@olash.com",
+      access: "No access",
+      type: "group",
+    },
+  ],
+  dataPerson: [
+    {
+      image: "/Logo.png",
+      name: "Tom Hank",
+      email: "tom@olash.com",
+      access: "No access",
+    },
+    {
+      image: "/Logo.png",
+      name: "Peter Bellosh",
+      email: "peter@olash.com",
+      access: "No access",
+    },
+  ],
+  dataGroup: [
+    {
+      image: "/Logo.png",
+      name: "Engineering",
+      members: "25",
+      email: "oslash@olash.com",
+      access: "No access",
+    },
+  ],
+};
